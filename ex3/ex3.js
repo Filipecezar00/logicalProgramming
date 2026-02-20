d0c44589ff160766eeb9f8d3f58915c85b5d8dec
filipe <cezar.filipe01@gmail.com>
2026-02-20 15:25:55 -0300

    atualizando

---
+++ ex3/ex3.js
@@ -0,0 +1,48 @@
+const btn_Adicionar = document.getElementById("btn_Adicionar"); 
+const btn_Limpar = document.getElementById("btn_Limpar"); 
+const input_Tarefa = document.getElementById("input_tarefa"); 
+const lista_tarefas = document.getElementById("lista_tarefas"); 
+
+let lista = []
+
+window.onload = function(){
+    let tarefas = localStorage.getItem("Tarefas"); 
+    if(tarefas!==null){
+        lista=JSON.parse(tarefas); 
+    }
+    lista_tarefas.innerHTML=""
+
+    lista.forEach((tarefa)=>{
+    lista_tarefas.innerHTML+=`<li>${tarefa}</li>`
+    })
+}
+
+function adicionarTarefa(){
+
+    let inputValue = input_Tarefa.value 
+    lista.push(inputValue); 
+
+
+    lista_tarefas.innerHTML=""
+
+    lista.forEach((tarefa)=>{
+    let arrayTarefas = JSON.stringify(lista);  
+    localStorage.setItem("Tarefas",arrayTarefas); 
+    lista_tarefas.innerHTML+= `<li>${tarefa}</li>`
+    })
+
+}
+
+function limparTudo(){
+    lista=[];
+    localStorage.removeItem("Tarefas");
+    input_Tarefa.value=""
+    lista_tarefas.innerHTML=""
+}
+btn_Adicionar.addEventListener("click",()=>{
+    adicionarTarefa(); 
+})
+btn_Limpar.addEventListener("click",()=>{
+    limparTudo(); 
+})
+
