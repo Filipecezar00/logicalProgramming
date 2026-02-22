f61d13d4f49ffccadb95fc7807e9cc16bbcd9e9c
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-22 00:11:28 -0300

    update ex4/ex4.js

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -2,3 +2,14 @@ let andarAtual=0
 const filaDeDestinos=[];
 let estaMovimentando=false 
 let direcao="parado"||"subindo"
+
+
+
+function clicarNoBotao(andarSelecionado){
+  if(filaDeDestinos!=andarSelecionado){
+    filaDeDestinos=andarSelecionado
+    filaDeDestinos.sort(direcao)
+  }else if(estaMovimentando==false){
+    processarFila();
+  }
+}
