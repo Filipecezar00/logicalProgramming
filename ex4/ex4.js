ad68349d97b674818133114f1aa5a6c4505adaa7
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-22 18:44:51 -0300

    update ex4/ex4.js

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -22,12 +22,13 @@ function processarFila(){
   setTimeout(function(){
     andarAtual=proximoAndar
     let removendo = filaDeDestinos.shift();
+    processarFila(); 
   },1000)
   
 }
 
 function clicarNoBotao(andarSelecionado){
-  if(filaDeDestinos!=andarSelecionado){
+  if(filaDeDestinos.includes(andarSelecionado){
     filaDeDestinos.push(andarSelecionado)
     filaDeDestinos.sort(direcao)
   }else if(estaMovimentando==false){
@@ -35,11 +36,21 @@ function clicarNoBotao(andarSelecionado){
   }
 }
 document.getElementById("btn_um").addEventListener('click',()=>clicarNoBotao(0));
-
+ 
+ processarFila(); 
+ 
 document.getElementById("btn_dois").addEventListener('click',()=>clicarNoBotao(1));
+
+processarFila();
 
 document.getElementById("btn_tres").addEventListener('click',()=>clicarNoBotao(2));
 
+processarFila();
+
 document.getElementById("btn_quatro").addEventListener('click',()=>clicarNoBotao(3));
 
+processarFila();
+
 document.getElementById("btn_ cinco").addEventListener('click',()=>clicarNoBotao(4));
+
+processarFila();
