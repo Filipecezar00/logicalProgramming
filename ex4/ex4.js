c1741e46161c680fb36ad8b37fef864757eaf148
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-22 00:19:54 -0300

    update ex4/ex4.js

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -1,9 +1,24 @@
 let andarAtual=0
 const filaDeDestinos=[];
 let estaMovimentando=false 
-let direcao="parado"||"subindo"
+let direcao="parado"||"subindo" || "descendo"
 
-
+function processarFila(){
+  if(filaDeDestinos.length==0){
+    estaMovimentando=false
+    direcao="parado"
+    return
+  }
+  estaMovimentando=true
+  let proximoAndar=filaDeDestinos[0];
+  
+  if(proximoAndar>andarAtual){
+    direcao="subindo"
+  }else if (proximoAndar<andarAtual){
+    direcao="descendo"
+  }
+  
+}
 
 function clicarNoBotao(andarSelecionado){
   if(filaDeDestinos!=andarSelecionado){
