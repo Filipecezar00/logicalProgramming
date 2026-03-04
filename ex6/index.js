a16f80cb30abee430d7504a970ad29577d66ae58
Filipecezar00 <cezarfilipe01@gmail.com>
2026-03-04 08:53:05 -0300

    atualizando

--- ex6/index.js
+++ ex6/index.js
@@ -1,12 +1,27 @@
 const despesas = [{}];
 const produtos = [{}];
 const resp = document.getElementById("resp");
+const input_valor = document.getElementById("valor");
+const input_despesa = document.getElementById("despesas");
+const btn_adicionar = document.getElementById("btn_adicionar");
 
 function CalcularMetas() {
-  let totalDespesas = despesas.reduce((acumulador, despesa));
+  let totalDespesas = despesas.reduce();
   produtos.forEach((precoVenda, precoCusto, produto) => {
     let lucro = Number(precoVenda - precoCusto);
     let unidadesNecessarias = Number(totalDespesas / lucro);
     resp.innerText = `Para pagar as contas apenas com ${produto.nome}, venda ${Math.ceil(unidadesNecessarias)} un.`;
   });
 }
+
+function AdicionarDespesa() {
+  let valor = input_valor.value;
+  let despesa = input_despesa.value;
+  despesas.push(despesa, valor);
+  localStorage.setItem("despesa", despesa);
+  CalcularMetas();
+}
+
+btn_adicionar.addEventListener("click", () => {
+  AdicionarDespesa();
+});
