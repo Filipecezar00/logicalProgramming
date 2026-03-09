35a47df32a5386706bbc5099c7ecbeef5e82d0d6
Filipecezar00 <cezarfilipe01@gmail.com>
2026-03-08 21:47:26 -0300

    atualizando

--- ex6/index.js
+++ ex6/index.js
@@ -1,27 +1,42 @@
-const despesas = [{}];
-const produtos = [{}];
+const despesas = [];
+const produtos = [{ nome: "Caneca", precoCusto: 200, precoVenda: 350 }];
 const resp = document.getElementById("resp");
 const input_valor = document.getElementById("valor");
 const input_despesa = document.getElementById("despesas");
 const btn_adicionar = document.getElementById("btn_adicionar");
 
 function CalcularMetas() {
-  let totalDespesas = despesas.reduce();
-  produtos.forEach((precoVenda, precoCusto, produto) => {
-    let lucro = Number(precoVenda - precoCusto);
-    let unidadesNecessarias = Number(totalDespesas / lucro);
-    resp.innerText = `Para pagar as contas apenas com ${produto.nome}, venda ${Math.ceil(unidadesNecessarias)} un.`;
+  let htmlFinal = "";
+  let totalDespesas = despesas.reduce((acc, item) => acc + item.valor, 0);
+  produtos.forEach((produto) => {
+    let lucro = Number(produto.precoVenda - produto.precoCusto);
+    let unidadesNecessarias = totalDespesas / lucro;
+
+    console.log(unidadesNecessarias);
+    console.log(produto);
+
+    htmlFinal += `<p>Para pagar as contas apenas com ${produto.nome} <br> venda ${Math.ceil(unidadesNecessarias)} un.</p>`;
+    resp.innerHTML = htmlFinal;
+    return;
   });
 }
 
 function AdicionarDespesa() {
-  let valor = input_valor.value;
-  let despesa = input_despesa.value;
-  despesas.push(despesa, valor);
+  let valor = Number(input_valor.value);
+  let despesa = Number(input_despesa.value);
+
+  const despesa_obj = {
+    nome: despesa,
+    valor: valor,
+  };
+
+  despesas.push(despesa_obj);
+  produtos.push();
   localStorage.setItem("despesa", despesa);
   CalcularMetas();
 }
 
 btn_adicionar.addEventListener("click", () => {
   AdicionarDespesa();
+  CalcularMetas();
 });
