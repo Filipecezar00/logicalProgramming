210d29cad87e99b2693d286a8ab82daa8a71b521
Filipecezar00 <cezarfilipe01@gmail.com>
2026-03-09 23:11:58 -0300

    atualizando

--- ex6/index.js
+++ ex6/index.js
@@ -3,7 +3,12 @@ const produtos = [{ nome: "Caneca", precoCusto: 200, precoVenda: 350 }];
 const resp = document.getElementById("resp");
 const input_valor = document.getElementById("valor");
 const input_despesa = document.getElementById("despesas");
+const precoCusto = document.getElementById("preco_custo");
+const precoVenda = document.getElementById("preco_venda");
 const btn_adicionar = document.getElementById("btn_adicionar");
+const btn_registrar = document.getElementById("btn_registrar");
+const produto = document.getElementById("produto");
+const quantidade = document.getElementById("quantidade");
 let lucroAcumulado = 0;
 
 function CalcularMetas() {
@@ -38,6 +43,28 @@ function AdicionarDespesa() {
   despesas.push(despesa_obj);
   localStorage.setItem("despesa", despesa);
   CalcularMetas();
+}
+
+function RegistrarVenda() {
+  let produto_value = produto.value;
+  let custo_value = Number(precoCusto.value);
+  let venda_value = Number(precoVenda.value);
+  let quantidade_value = Number(quantidade.value);
+
+  if (venda_value <= custo_value) {
+    alert("Operação com Prejuízo, verifique os preços");
+    return;
+  }
+  let lucroUnitario = venda_value - custo_value;
+  let lucroVendaAtual = lucroUnitario * quantidade_value;
+  lucroAcumulado = lucroAcumulado + lucroVendaAtual;
+
+  localStorage.setItem("lucroAcumulado", lucroAcumulado);
+
+  produto_value = "";
+  custo_value = "";
+  venda_value = "";
+  quantidade_value = "";
 }
 
 btn_adicionar.addEventListener("click", () => {
