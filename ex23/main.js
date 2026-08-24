6e1f2bdc3cd4c62272edb62fb1218ed6f5346c97
Filipecezar00 <cezarfilipe01@gmail.com>
2026-08-24 09:40:20 -0300

    atualizando

--- ex23/main.js
+++ ex23/main.js
@@ -7,9 +7,20 @@ const lista_livros = document.getElementById("lista_livros");
 const error = document.getElementById("error");
 
 async function CadastrarLivro() {
+  let titulo_value = titulo.value;
+  let ano_value = Number(ano.value);
+  let autor_value = autor.value;
+
   try {
-    const cadastro = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const resposta = JSON.parse(cadastro.json);
+    const req_post = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        titulo: titulo_value,
+        ano: ano_value,
+        autor: autor_value,
+      }),
+    });
+    const resposta = JSON.parse(req_post.json);
 
     error.innerHTML = `Livro : ${resposta} cadastrado com sucesso !`;
 
@@ -21,11 +32,36 @@ async function CadastrarLivro() {
 
 async function ListarLivros() {
   try {
-    const listagem = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const resposta = JSON.parse(listagem.json);
+    const req_get = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const resposta = JSON.parse(req_get.json);
 
     lista_livros.innerHTML += `<li>${resposta}</li>`;
   } catch (error) {
     console.log("ERRO AO LISTAR LIVROS:", error);
+  }
+}
+
+async function editarLivro(id) {
+  let titulo_value = titulo.value;
+  let ano_value = Number(ano.value);
+  let autor_value = autor.value;
+  try {
+    const req_put = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          titulo: titulo_value,
+          ano: ano_value,
+          autor: autor_value,
+        }),
+      },
+    );
+    const resposta = JSON.parse(req_put.json());
+
+    error.innerHTML = `Editado com sucesso!`;
+    lista_livros.innerHTML += `<li>${resposta}</li>`;
+  } catch (error) {
+    console.log("ERRO AO EDITAR LIVRO:", error);
   }
 }
