68273d62f65e755bc908ed2d24e9179bba0b1c08
Filipecezar00 <cezarfilipe01@gmail.com>
2026-08-24 08:59:58 -0300

    atualizando

---
+++ ex23/main.js
@@ -0,0 +1,20 @@
+const form = document.getElementById("form");
+const titulo = document.getElementById("titulo");
+const autor = document.getElementById("autor");
+const ano = document.getElementById("ano");
+const btn_enviar = document.getElementById("btn_enviar");
+const lista_livros = document.getElementById("lista_livros");
+const error = document.getElementById("error");
+
+async function CadastrarLivro() {
+  try {
+    const cadastro = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const resposta = JSON.parse(cadastro.json);
+
+    error.innerHTML = `Livro : ${resposta} cadastrado com sucesso !`;
+
+    lista_livros.innerHTML += `<li>${resposta}</li>`;
+  } catch (error) {
+    console.log("ERRO AO CADASTRAR LIVRO:", error);
+  }
+}
